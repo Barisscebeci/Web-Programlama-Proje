bb08b93d8eaef1c7eb3f5dce5831203974a4d209
Barisscebeci <104255155+Barisscebeci@users.noreply.github.com>
2023-12-27 21:02:38 +0300

    Add files via upload

---
+++ WebProje/WebProje/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using HastaneBilgiSistemi.Cache;
+using HastaneBilgiSistemi.CookieOptions;
+using HastaneBilgiSistemi.Data;
+using HastaneBilgiSistemi.Data.Model;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+[assembly: HostingStartup(typeof(HastaneBilgiSistemi.Areas.Identity.IdentityHostingStartup))]
+namespace HastaneBilgiSistemi.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+
+                
+            });
+        }
+    }
+}
